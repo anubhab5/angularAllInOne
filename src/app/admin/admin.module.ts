b2f39d8f75ea5e7b85320e37d9e8620401ca9212
Anubhab Mukherjee <anubhabmukherjee5@gmail.com>
2020-06-11 23:56:03 +0530

    New Pipes service component added

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { UppercasePipe } from './uppercase.pipe';
+import { LowercasePipe } from './lowercase.pipe';
+import { RandomCompComponent } from './random-comp/random-comp.component';
 
 
-
+// ng g pipe Lowercase --module admin
 @NgModule({
   declarations: [
-    AdminDashboardComponent
+    AdminDashboardComponent,
+    RandomCompComponent,
+    UppercasePipe,
+    LowercasePipe
   ],
   imports: [
     CommonModule
