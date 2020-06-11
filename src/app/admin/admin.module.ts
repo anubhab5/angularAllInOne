4fe6da3d11640ae9d06871aad53318632932bb54
Anubhab Mukherjee <anubhabmukherjee5@gmail.com>
2020-06-11 23:30:51 +0530

    Created Modules

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+
+
+
+@NgModule({
+  declarations: [
+    AdminDashboardComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  bootstrap: [],
+  exports:[
+    AdminDashboardComponent
+  ]
+})
+export class AdminModule { }
